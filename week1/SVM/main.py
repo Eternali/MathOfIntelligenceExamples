efcc1dd216a4ec2c2303c100ed57909f837ed48d
conrad <conrad.heidebrecht@gmail.com>
2017-08-03 00:46:47 -0400

    second commit

--- week1/SVM/main.py
+++ week1/SVM/main.py
@@ -1,8 +1,20 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+# input data - of the form [X value, Y value, Bias term]
 X = np.array([
-    [-2,4,-1],
-    
+    [-2, 4, -1],
+    [4, 1, -1],
+    [1, 6, -1],
+    [2, 4, -1],
+    [6, 2, -1]
 ])
 
+# associated output labels
+y = np.array([-1, -1, 1, 1, 1])
+
+# plot these examples on a 2D graph
+for d, sample in enumerate(X):
+    # plot negative samples
+    if d < 2:
+        plt.scatter(sample[0], sample[1], s=120, marker='_', linewidths=)
